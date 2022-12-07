d96a99f865be010c00673f90cec6288ba478187d
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-12-07 17:20:29 +0900

    Feat: Newnote component

--- src/componets/NoteForm.tsx
+++ src/componets/NoteForm.tsx
@@ -1,5 +1,72 @@
-const NoteForm = () => {
-    return <h1>form</h1>
+import { Link } from "react-router-dom";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable"
+import { FormEvent, useRef, useState } from "react";
+import { NoteData, Tag } from "../App";
+
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+}
+
+const NoteForm = ({ onSubmit} : NoteFormProps) => {
+    const titleRef =useRef<HTMLInputElement>(null)
+    const markdownRef = useRef<HTMLTextAreaElement>(null)
+
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+const handleSubmit =( e: FormEvent) =>{
+    e.preventDefault();
+
+onSubmit({
+    title: titleRef.current!.value,
+    markdown : markdownRef.current!.value,
+    tags:[]
+})
+
+}
+
+    return( 
+    <Form onSubmit={handleSubmit}>
+        <Stack>
+            <Row>
+                <Col>
+                <Form.Group controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control ref={titleRef } required/>
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group controlId="title">
+                <Form.Label>Tags</Form.Label>
+                <CreatableReactSelect value={selectedTags.map(tag =>{
+                    return {label: tag.label, value: tag.id}
+                })}
+                onChange={tags =>{
+                    setSelectedTags(tags.map(tag =>{
+                        return {label: tag.label, id:tag.value}
+                    }))
+                }}
+                 isMulti/>
+                </Form.Group>
+                </Col>
+            </Row>
+            <Form.Group controlId="markdown">
+            <Form.Label>Body</Form.Label>
+            <Form.Control required as="textarea" ref={markdownRef} rows={15}/>
+            <Stack direction="horizontal" gap={2}>
+                <Button type="submit" variant="primary">Save</Button>
+                <Link to="..">
+                
+                <Button type="button" variant="outline-secondary">Cancel</Button>
+                </Link>
+
+            </Stack>
+            </Form.Group>
+         </Stack>
+        
+      
+    </Form>
+    )
 }
  
 export default NoteForm;
