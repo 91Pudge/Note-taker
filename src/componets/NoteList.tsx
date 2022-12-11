1f7c63e8c1ece634b4cad753a6a39bdd64e12ab9
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-12-11 16:58:38 +0900

    Feat: Create note list component

---
+++ src/componets/NoteList.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Button, Col, Row, Stack, Form } from "react-bootstrap";
+import {  Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { NoteData, Tag } from "../App";
+
+
+type NoteListProps = {
+    availableTags: Tag[]
+}
+
+const NoteList = ({availableTags}: NoteListProps) => {
+
+    const [selectedTags, setSelectedTags] = useState<Tag[]> ([])
+    const [title, setTitle] = useState<string> ()
+
+    return (  <>
+ 
+    <Row className="align-items-center mb-4">
+        <Col><h1>List</h1></Col>
+        <Col xs="auto">
+            <Stack gap={2} direction="horizontal"></Stack>
+            <Link to="/new">
+            <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit</Button>
+        </Col>
+    </Row>
+    <Form>
+        <Row className="mb-4">
+            <Col>
+            <Form.Group controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" value={title} onChange={e =>(setTitle(e.target.value))} />
+            </Form.Group>
+            </Col>
+            <Col>
+            <Form.Group controlId="title">
+                <Form.Label>Tags</Form.Label>
+                <ReactSelect
+            
+                value={selectedTags.map(tag =>{
+                    return {label: tag.label, value: tag.id}
+                })}
+                options={availableTags.map(tag =>{
+                    return  {label: tag.label, value: tag.id}
+                })}
+                onChange={tags =>{
+                    setSelectedTags(tags.map(tag =>{
+                        return {label: tag.label, id:tag.value}
+                    }))
+                }}
+                 isMulti/>
+                </Form.Group></Col>
+        </Row>
+        </Form>    
+    
+    
+    </>);
+}
+ 
+export default NoteList;
